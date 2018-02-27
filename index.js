796d7dcda5ddbbf90281d078fc416f727aaa3826
Gautier ROCHAS <gautier.rochas@advency.fr>
2018-02-27 14:22:57 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+var MailListener = require("mail-listener2");
+
+module.exports = function(infos, callback) {
+
+	for (var i in infos) {
+
+		var info = infos[i];
+
+		var mailListener = new MailListener({
+			username: info.login,
+			password: info.pass,
+			host: info.imap,
+			port: 993,
+			tls: true,
+			tlsOptions: {
+				rejectUnauthorized: false
+			},
+			markSeen: false,
+			mailbox: "INBOX",
+		});
+
+		mailListener.start();
+
+		mailListener.on("mail", function(mail, seqno, attributes) {
+			callback(mail, info.login);
+		});
+
+	}
+
+};
